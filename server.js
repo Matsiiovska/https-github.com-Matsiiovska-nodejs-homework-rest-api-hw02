c3de48a1b18c61b041f89fc91c9d47ac088b515b
Matsiiovska <125771191+Matsiiovska@users.noreply.github.com>
2023-11-22 02:12:11 +0300

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app'); 
 const mongoose = require('mongoose');
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 
 mongoose.set('strictQuery', true);
@@ -8,7 +8,7 @@ mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOST)
   .then(() => {
     console.log('Database connection successful');
-    app.listen(3000);
+    app.listen(PORT);
   }
   )
   .catch(error => {
